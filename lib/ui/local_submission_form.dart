647c7509bb8d8766ddd9d4670c1de6d8a2357a4b
Oscar Henry Torres Caballero <oscar.torres@lighthousetech.io>
2023-06-14 00:03:33 -0400

    - refact

--- lib/ui/local_submission_form.dart
+++ lib/ui/local_submission_form.dart
@@ -1,7 +1,7 @@
-import 'package:astech_demo/custom_text_field.dart';
-import 'package:astech_demo/custom_switch_field.dart';
-import 'package:astech_demo/field.dart';
-import 'package:astech_demo/formatter.dart';
+import 'package:astech_demo/customs/custom_text_field.dart';
+import 'package:astech_demo/customs/custom_switch_field.dart';
+import 'package:astech_demo/commons/field.dart';
+import 'package:astech_demo/commons/formatter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
